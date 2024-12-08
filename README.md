35125e5a67d8949d4daa3f807687a5bb85b21105
mattia <zingaretti.mattia@gmail.com>
2024-12-08 17:29:26 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# canvas_crawler
-Get Pdf files and external resources for each course provided
+# 🎓 Canvas Crawler
+
+Welcome to **Canvas Crawler**! 🚀 This tool helps you fetch PDF files and external resources for each course provided on Canvas. <br>
+Say goodbye to manual downloads and hello to automation! 🎉
+
+## 📋 Requirements
+
+Before you dive in, make sure you have the following:
+
+- **Node.js v20 or later** installed. 🟢
+- A `downloads` folder in the root directory where all your course goodies will be saved. 📂
+- A `.env` file in the root directory with your Canvas access token. Don't worry, I won't peek! unless you push it here 🤫
+  ```dotenv
+  CANVAS_API_KEY=your_canvas_access_token (you can create one from your canvas account settings)
+  CANVAS_API_URL=your_canvas_base_api_url
+- A `coursesMapping.json` file in the root directory with the following content:
+```json
+{
+  "courseIdToDescriptions": [
+    {
+      "code": "The Canvas Code for the course you want to extract pdf, external links",
+      "descr": "A description of the course id that will be used to name the subdir in which you ll find the files"
+    }
+  ]
+}
+```
+
+## 🚀 Installation & Usage
+Ready to get started? Follow these steps:
+- Clone the repository:`git clone https://github.com/zingarettimattia/canvas_crawler.git`
+- Navigate to the project directory: `cd canvas_crawler`
+- Verify that you meet the Requirements described above. ✅
+- Install the dependencies: `npm install`
+- Start the crawler:`npm run start`
+
+And that's it! Sit back, relax, and let Canvas Crawler do the heavy lifting. 🛋️
+
+## 📚 How It Works
+Canvas Crawler will:
+
+- Fetch course modules from Canvas from the info provided in the `coursesMapping.json` file.
+- Download PDF files and save them in corresponding downloads sub folder.
+- Collect external URLs and save them in a single CSV file.
---
+++ index.ts
@@ -0,0 +1,128 @@
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
+
+dotenv.config();
+
+const canvasApiToken = process.env.CANVAS_API_TOKEN;
+const canvasBaseUrl = process.env.CANVAS_BASE_URL;
+
+
+
+if (!canvasApiToken || !canvasBaseUrl) {
+    throw new Error('Missing Canvas API token or base URL in environment variables');
+}
+const filePath = path.join(__dirname, 'coursesMapping.json');
+if (!fs.existsSync(filePath)) {
+    throw new Error(`Please create a JSON file here, see Readme for details...  ${filePath}`);
+}
+const fileContent = fs.readFileSync(filePath, 'utf8');
+const { courseIdToDescriptions } = JSON.parse(fileContent);
+
+async function getModules(courseId: string) {
+    const response = await axios.get(`${canvasBaseUrl}/courses/${courseId}/modules`, {
+        headers: {
+            'Authorization': `Bearer ${canvasApiToken}`
+        }
+    });
+    return response.data;
+}
+
+async function getModuleItems(courseId: string, moduleId: string) {
+    const response = await axios.get(`${canvasBaseUrl}/courses/${courseId}/modules/${moduleId}/items`, {
+        headers: {
+            'Authorization': `Bearer ${canvasApiToken}`
+        }
+    });
+    return response.data;
+}
+
+async function downloadPDF(url: string, fileName: string, courseId: string): Promise<void> {
+    try {
+        if(!courseId){
+            throw new Error('Missing courseId');
+        }
+        const downloadsFolder = path.join(__dirname, `downloads/${courseId}`);
+        if (!fs.existsSync(downloadsFolder)) {
+            fs.mkdirSync(downloadsFolder);
+        }
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${canvasApiToken}`
+            },
+            responseType: 'arraybuffer',
+        });
+        //Avoid fancy naming by professors...
+        fileName = fileName.replace(/[^\x00-\x7F]/g, '_').replace(/\//g, '_').toLowerCase();
+        const filePath = path.join(downloadsFolder, fileName);
+        fs.writeFileSync(filePath, response.data);
+        console.log(`File saved: ${filePath}`);
+    } catch (error) {
+        console.error('Error downloading PDF:', error);
+    }
+}
+
+async  function getSlidesUrl(item: any){
+    try{
+        const response = await axios.get(item.url, {
+            headers: {
+                'Authorization': `Bearer ${canvasApiToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    }catch (error){
+        console.error(error)
+        return null;
+    }
+}
+
+async function savePdfFiles(courseId: string, courseDescr: string) {
+    const modules = await getModules(courseId);
+    const externalUrls: { url: string, title: string }[] = [];
+
+    for (const module of modules) {
+        const moduleItems = await getModuleItems(courseId, module.id);
+        for (const item of moduleItems) {
+
+            if (item.type === 'File' ) {
+                getSlidesUrl(item).then((response) => {
+                   if(response != null){
+                       downloadPDF(response.url, item.title + '.'+ response.mime_class, courseDescr).catch(() => {
+                            console.error('Error downloading PDF');
+                       });
+                   }
+                });
+            }else if(item.type === 'ExternalUrl'){
+                externalUrls.push({url: item.external_url, title: item.title});
+            }
+        }
+    }
+    return externalUrls;
+}
+
+function createCSV(data: { url: string, title: string }[], item: {descr:string, code: string}) {
+    const csvLines = data.map(item => `${item.title},${item.url}`);
+    const csvContent = csvLines.join('\n');
+
+    const downloadsFolder = path.join(__dirname, `downloads`);
+    if (!fs.existsSync(downloadsFolder)) {
+        throw new Error("Please create a 'downloads' folder in the current directory");
+    }
+    const filePath = path.join(__dirname, `downloads/${item.descr}/000_External_Resources_${item.descr}.csv`)
+    fs.writeFileSync(filePath, csvContent, 'utf8');
+}
+
+
+
+(async () => {
+    try {
+        courseIdToDescriptions.forEach(async (item) => {
+            const externalUrls: {url: string, title: string}[]= await savePdfFiles(item.code, item.descr);
+            createCSV(externalUrls, item);
+        });
+    } catch (error) {
+        console.error('Error fetching PDF files:', error);
+    }
+})();
